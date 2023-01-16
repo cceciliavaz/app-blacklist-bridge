da58748cf6383b3ba257b4de7c4535b23e060a84
Ana Cecília Vaz <73395116+cceciliavaz@users.noreply.github.com>
2023-01-15 21:14:09 -0300

    retirando importação de material

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { SidebarModule } from 'app/components/sidebar/sidebar.module';
-import { MaterialModule } from '../shared/material.module';
 import { CreateAccountComponent } from './create-account/create-account.component';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [MainComponent, CreateAccountComponent],
+  declarations: [CreateAccountComponent],
   imports: [
-    RouterModule.forChild([{
-      path: '',
-      pathMatch: 'full',
-      component: MainComponent
-    }]),
-    MaterialModule,
     SidebarModule
   ],
   providers: [],
